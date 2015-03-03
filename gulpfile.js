3d9491e923f6a5ba369c242592e314f9039546ec
Jane <janecakemaster@gmail.com>
2015-03-03 00:45:53 -0500

    use uglify too

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var gulp = require('gulp'),
     autoprefixer = require('gulp-autoprefixer'),
     minifycss = require('gulp-minify-css'),
     jshint = require('gulp-jshint'),
+    uglify = require('gulp-uglify'),
     rename = require('gulp-rename'),
     concat = require('gulp-concat'),
     notify = require('gulp-notify'),
@@ -11,28 +12,39 @@ var gulp = require('gulp'),
     del = require('del');
 
 gulp.task('sass', function() {
-  return sass('src/sass/main.scss', { style: 'expanded' })
-    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-    .pipe(gulp.dest('dist/assets/css'))
-    .pipe(rename({suffix: '.min'}))
-    .pipe(minifycss())
-    .pipe(gulp.dest('dist/assets/css'))
-    .pipe(notify({ message: 'SASS task complete' }));
+    return sass('src/sass/main.scss', {
+            style: 'expanded'
+        })
+        .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+        .pipe(gulp.dest('dist/assets/css'))
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(minifycss())
+        .pipe(gulp.dest('dist/assets/css'))
+        .pipe(notify({
+            message: 'SASS task complete'
+        }));
 });
 
 gulp.task('js', function() {
-  return gulp.src('src/js/**/*.js')
-    .pipe(jshint('.jshintrc'))
-    .pipe(jshint.reporter('default'))
-    .pipe(concat('main.js'))
-    .pipe(gulp.dest('dist/assets/js'))
-    .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('dist/assets/js'))
-    .pipe(notify({ message: 'JS task complete' }));
+    return gulp.src('src/js/**/*.js')
+        .pipe(jshint('.jshintrc'))
+        .pipe(jshint.reporter('default'))
+        .pipe(concat('main.js'))
+        .pipe(gulp.dest('dist/assets/js'))
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/assets/js'))
+        .pipe(notify({
+            message: 'JS task complete'
+        }));
 });
 
 gulp.task('clean', function(cb) {
-    del(['dist/assets/css', 'dist/assets/js'], cb);
+    del(['dist/assets/css', 'dist/assets/js', '.sass_cache'], cb);
 });
 
 gulp.task('default', ['clean'], function() {
@@ -40,9 +52,9 @@ gulp.task('default', ['clean'], function() {
 });
 
 gulp.task('watch', function() {
-  gulp.watch('src/sass/**/*.scss', ['sass']);
-  gulp.watch('src/js/**/*.js', ['js']);
+    gulp.watch('src/sass/**/*.scss', ['sass']);
+    gulp.watch('src/js/**/*.js', ['js']);
 
-  livereload.listen();
-  gulp.watch(['dist/**']).on('change', livereload.changed);
+    livereload.listen();
+    gulp.watch(['dist/**']).on('change', livereload.changed);
 });
