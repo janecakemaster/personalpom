bcc6ac8b49f8d2aa8557c0ed0fd2a0d35fa5b7ef
Jane <janecakemaster@gmail.com>
2015-03-03 00:13:26 -0500

    use gulp yah

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,49 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    sass = require('gulp-ruby-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifycss = require('gulp-minify-css'),
+    jshint = require('gulp-jshint'),
+    rename = require('gulp-rename'),
+    concat = require('gulp-concat'),
+    notify = require('gulp-notify'),
+    cache = require('gulp-cache'),
+    livereload = require('gulp-livereload'),
+    del = require('del');
 
-gulp.task('default', function() {
-  // place code for your default task here
+gulp.task('sass', function() {
+  return gulp.src('src/sass/main.scss')
+    .pipe(sass({ style: 'expanded' }))
+    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+    .pipe(gulp.dest('dist/assets/css'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(minifycss())
+    .pipe(gulp.dest('dist/assets/css'))
+    .pipe(notify({ message: 'SASS task complete' }));
+});
+
+gulp.task('js', function() {
+  return gulp.src('src/js/**/*.js')
+    .pipe(jshint('.jshintrc'))
+    .pipe(jshint.reporter('default'))
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('dist/assets/js'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('dist/assets/js'))
+    .pipe(notify({ message: 'JS task complete' }));
+});
+
+gulp.task('clean', function(cb) {
+    del(['dist/assets/css', 'dist/assets/js'], cb);
+});
+
+gulp.task('default', ['clean'], function() {
+    gulp.start('sass', 'js');
+});
+
+gulp.task('watch', function() {
+  gulp.watch('src/sass/**/*.scss', ['sass']);
+  gulp.watch('src/js/**/*.js', ['js']);
+
+  livereload.listen();
+  gulp.watch(['dist/**']).on('change', livereload.changed);
 });
