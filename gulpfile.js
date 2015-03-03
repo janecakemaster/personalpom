e504c19999a7d4dde4e9453f62ce9c14b9d1bf1c
Jane <janecakemaster@gmail.com>
2015-03-03 00:40:05 -0500

    update gitinore and first scss build

--- gulpfile.js
+++ gulpfile.js
@@ -11,8 +11,7 @@ var gulp = require('gulp'),
     del = require('del');
 
 gulp.task('sass', function() {
-  return gulp.src('src/sass/main.scss')
-    .pipe(sass({ style: 'expanded' }))
+  return sass('src/sass/main.scss', { style: 'expanded' })
     .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
     .pipe(gulp.dest('dist/assets/css'))
     .pipe(rename({suffix: '.min'}))
